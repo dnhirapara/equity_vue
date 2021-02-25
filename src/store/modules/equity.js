71a4fcc230c45bd03eddf8df15df4a3e303e82f0
Darshik N. Hirapara <43730525+dnhirapara@users.noreply.github.com>
2021-02-25 12:02:52 +0530

    Update equity.js

--- src/store/modules/equity.js
+++ src/store/modules/equity.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = "https://bhavcopy-1.herokuapp.com"
 const state = {
     data: [],
     dataPerScroll: 100,
